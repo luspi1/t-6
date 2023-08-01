e4b87affc17913f59cfbb20a6d961ceffc14cffc
Kramskoy-v <kramskoy.va@gmail.com>
2023-08-01 17:33:40 +0300

    Новая стр profile-user.html десктоп

--- src/js/components/dropzone.js
+++ src/js/components/dropzone.js
@@ -1,43 +1,44 @@
-import { Dropzone } from "dropzone"
+import {Dropzone} from 'dropzone'
 
 //Dropzone для Несколько сторон участников
 
 const commandImgs = document.querySelectorAll('.command-dropzone')
 
-  if (commandImgs) {
+if (commandImgs) {
     commandImgs.forEach(commandImg => {
-      const addBtnDropzone = commandImg.querySelector('.big-form__command-btn-dropzone')
+        const addBtnDropzone = commandImg.querySelector('.big-form__command-btn-dropzone')
 
-      let commandImgDropzone = new Dropzone(commandImg, {
-        maxFilesize: 5,
-        url: '/include/ajax/upload_image.php',
-        maxFiles: 1,
-        thumbnailWidth: 15,
-        thumbnailHeight: 15,
-        acceptedFiles: '.png, .jpeg, .jpg',
-        addRemoveLinks: true,
-        dictRemoveFile: 'удалить',
-        clickable: addBtnDropzone,
-      })
+        let commandImgDropzone = new Dropzone(commandImg, {
+            maxFilesize: 5,
+            url: '/include/ajax/upload_image.php',
+            maxFiles: 1,
+            thumbnailWidth: 15,
+            thumbnailHeight: 15,
+            acceptedFiles: '.png, .jpeg, .jpg',
+            addRemoveLinks: true,
+            dictRemoveFile: 'удалить',
+            clickable: addBtnDropzone,
+        })
     })
- }
+}
 
 
 //Загрузка логотипа
 
-const organizerProfileLogo = document.querySelector('#profile-organizer-logo')
+const organizerProfileLogo = document.querySelector('#profile-logo')
 
 if (organizerProfileLogo) {
-      let organizerLogoDropzone = new Dropzone(organizerProfileLogo, {
-      maxFilesize: 5,
-      url: '/include/ajax/upload_image.php',
-      maxFiles: 1,
-      thumbnailWidth: 108,
-      thumbnailHeight: 108  ,
-      acceptedFiles: '.png, .jpeg, .jpg',
-      addRemoveLinks: true,
-      dictRemoveFile: 'удалить',
-      clickable: '.big-form__btn-add-logo',
+    let organizerLogoDropzone = new Dropzone(organizerProfileLogo, {
+        maxFilesize: 5,
+        url: '/include/ajax/upload_image.php',
+        maxFiles: 1,
+        thumbnailWidth: 108,
+        thumbnailHeight: 108,
+        acceptedFiles: '.png, .jpeg, .jpg, .svg',
+        addRemoveLinks: true,
+        dictRemoveFile: 'удалить',
+        clickable: '.big-form__btn-add-logo',
+        previewsContainer: '.big-form__dropzone-previews'
     })
 
 }
