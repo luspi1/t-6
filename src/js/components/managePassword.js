9766f446d906ff165a23932b252bc560c14846cf
ludaalt <luda.tamb@mail.ru>
2023-08-15 11:32:11 +0300

    правки по PR

--- src/js/components/managePassword.js
+++ src/js/components/managePassword.js
@@ -2,27 +2,29 @@ const showPasswordButton = document.querySelector('.show-password')
 const password = document.querySelector('#password')
 const submitEnterButton = document.querySelector('.submit-enter')
 
-const resetPasswordBlock = document.querySelector('.reset-passsword-block')
+const resetPasswordForm = document.querySelector('.reset-password-form')
 const resetPasswordButton = document.querySelector('.reset-password-button')
 
 const smsTimer = document.querySelector('#smsTimer')
 let smsSeconds = (smsTimer.innerHTML).split(":")[1]
 
+if(showPasswordButton) {
+    showPasswordButton.addEventListener('click', () => {
+        password.type = password.type === "password" ? "text" : "password"      
+    })
+}
 
-showPasswordButton.addEventListener('click', () => {
-    password.type = password.type === "password" ? "text" : "password"      
-})
-
-
-resetPasswordButton.addEventListener('click', () => {
-    submitEnterButton.classList.add('disabled')  
-
-    resetPasswordBlock.style.display = 'block'
-
-    const smsIntervalId = setInterval(() => {
-        smsSeconds --;
-        if(smsSeconds === 0) clearInterval(smsIntervalId);
-        smsTimer.innerHTML = `0:${smsSeconds}`;                
-        
-    }, 1000)
-})
+if(resetPasswordButton) {
+    resetPasswordButton.addEventListener('click', () => {
+        submitEnterButton.classList.add('disabled')  
+    
+        resetPasswordForm.style.display = 'block'
+    
+        const smsIntervalId = setInterval(() => {
+            smsSeconds --;
+            if(smsSeconds === 0) clearInterval(smsIntervalId);
+            smsTimer.innerHTML = `0:${smsSeconds}`;                
+            
+        }, 1000)
+    })
+}
