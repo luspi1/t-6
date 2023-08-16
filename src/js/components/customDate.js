d53a1d30454785497565e1fea4dc990de0830be9
ludaalt <luda.tamb@mail.ru>
2023-08-16 13:02:20 +0300

    скорректировано отображение календаря

--- src/js/components/customDate.js
+++ src/js/components/customDate.js
@@ -1,9 +1,28 @@
-import AirDatepicker from "air-datepicker"
+import AirDatepicker from 'air-datepicker';
 
+const initAllDates = () => {
+  const allDateInputs = document.querySelectorAll('.custom-date');
 
-const customDates = document.querySelectorAll('.custom-date')
+  if (allDateInputs) {
+    allDateInputs.forEach((el) => {
+      const customDate = new AirDatepicker(el, {
+        container: '.date-custom-container',
+      });
 
-customDates.forEach(date=> new AirDatepicker(date))
+      el.addEventListener('click', (e) => {
+        const featuredDate = e.currentTarget.value
+          .split('.')
+          .reverse()
+          .join('-');
+        if (featuredDate) {
+          customDate.selectDate(featuredDate);
+          customDate.setViewDate(featuredDate);
+        }
+      });
+    });
+  }
+};
 
+initAllDates();
 
-
+export { initAllDates };
