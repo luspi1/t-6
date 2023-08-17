8ee99ad97b3586b8395482b1641ba25b31ce1eb3
ludaalt <luda.tamb@mail.ru>
2023-08-17 11:59:26 +0300

    выделение кнопки показа пароля в отдельный компонент

--- src/js/components/managePassword.js
+++ src/js/components/managePassword.js
@@ -1,22 +1,23 @@
-const showPasswordButton = document.querySelector('.show-password');
-const password = document.querySelector('#password');
-const submitEnterButton = document.querySelector('.submit-enter');
+const showPasswordButtons = document.querySelectorAll('.btn-show-pass');
 
+const submitEnterButton = document.querySelector('.submit-enter');
 const resetPasswordForm = document.querySelector('.reset-password-form');
 const resetPasswordButton = document.querySelector('.reset-password-button');
 
 const smsTimer = document.querySelector('#smsTimer');
 let smsSeconds = smsTimer.innerHTML.split(':')[1];
 
-if (showPasswordButton) {
-  showPasswordButton.addEventListener('click', () => {
-    if (password.type === 'password') {
-      password.type = 'text';
-      showPasswordButton.classList.add('_is-shown-password');
-    } else {
-      password.type = 'password';
-      showPasswordButton.classList.remove('_is-shown-password');
-    }
+if (showPasswordButtons) {
+  showPasswordButtons.forEach((item) => {
+    item.addEventListener('click', () => {
+      if (item.previousElementSibling.type === 'password') {
+        item.previousElementSibling.type = 'text';
+        item.classList.remove('_open');
+      } else {
+        item.previousElementSibling.type = 'password';
+        item.classList.add('_open');
+      }
+    });
   });
 }
 
