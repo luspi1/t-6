e2457672e073b0895844b94109ea0dcfc7cf03c9
ludaalt <luda.tamb@mail.ru>
2023-08-25 15:29:22 +0300

    скорректирована функция

--- src/js/components/bigForm.js
+++ src/js/components/bigForm.js
@@ -1,10 +1,3 @@
-import {
-  limitationChangeableElements,
-  updateChangeableListId,
-} from '../_functions';
-//import { initSelects } from './customSelect';
-//import { initDrop } from './dropzone';
-
 //отображение новой площадки на create-event
 const showPlaceBtn = document.querySelector('.big-form__btn-new-place');
 const newPlace = document.querySelector('.big-form__new-place');
@@ -104,49 +97,6 @@ if (showCheckboxes) {
     checkbox.addEventListener('change', checkedState);
   });
 }
-
-//template
-const changeableLists = document.querySelectorAll('ul[data-list="changeable"]');
-
-// Удаление элементов в изменяемых списках
-if (changeableLists) {
-  changeableLists.forEach((list) => {
-    list.addEventListener('click', (e) => {
-      if (e.target.dataset.btn === 'delete') {
-        e.target.closest('li').remove();
-        const addBtn = list.parentElement.querySelector(
-          'button[data-btn="add"]',
-        );
-        updateChangeableListId(list);
-        limitationChangeableElements(list, addBtn);
-      }
-    });
-  });
-}
-
-// Добавление элементов в изменяемых списках
-// const addToListBtns = document.querySelectorAll('button[data-btn="add"]');
-
-// if (addToListBtns) {
-//   addToListBtns.forEach((addBtn) => {
-//     addBtn.addEventListener('click', (e) => {
-//       e.preventDefault();
-//       const templateId = e.currentTarget.dataset.template;
-//       let templateFragment = document.querySelector(`#${templateId}`)?.content;
-//       let templateElement = templateFragment.firstElementChild.cloneNode(true);
-//       const targetChangeableList = e.currentTarget.parentElement.querySelector(
-//         'ul[data-list="changeable"]',
-//       );
-//       targetChangeableList.appendChild(templateElement);
-//       limitationChangeableElements(targetChangeableList, addBtn);
-//       updateChangeableListId(targetChangeableList);
-//       initSelects();
-//       if (tickets) {
-//         showTicketsBlock();
-//       }
-//     });
-//   });
-// }
 
 //валидация пароля
 
